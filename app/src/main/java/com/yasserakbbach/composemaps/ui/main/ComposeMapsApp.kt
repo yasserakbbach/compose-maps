cd6a7f720d113ed177ba0a50542291c2c8b14c96
Yasser Akbbach <akbbach.yasser@gmail.com>
2024-10-12 17:40:47 +0300

    setup DI

---
+++ app/src/main/java/com/yasserakbbach/composemaps/ui/main/ComposeMapsApp.kt
@@ -0,0 +1,8 @@
+package com.yasserakbbach.composemaps.ui.main
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ComposeMapsApp() {
+
+}
---
+++ app/src/main/java/com/yasserakbbach/composemaps/ui/main/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.yasserakbbach.composemaps.ui.main
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.yasserakbbach.composemaps.ui.theme.ComposeMapsTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposeMapsTheme {
+                ComposeMapsApp()
+            }
+        }
+    }
+}
