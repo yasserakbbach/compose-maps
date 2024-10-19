523fc7030c354767479bbb6533657c551325737a
Yasser Akbbach <akbbach.yasser@gmail.com>
2024-10-19 20:12:11 +0300

    support deeplink to open the maps by given link with longitude and latitude

--- app/src/main/java/com/yasserakbbach/composemaps/ui/maps/MapsNavigation.kt
+++ app/src/main/java/com/yasserakbbach/composemaps/ui/maps/MapsNavigation.kt
@@ -5,13 +5,21 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import androidx.navigation.navDeepLink
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class MapsRoute(val longitude: Double, val latitude: Double)
+private const val url = "composemaps.com"
 
 fun NavGraphBuilder.mapsScreen() {
-    composable<MapsRoute> {
+    composable<MapsRoute>(
+        deepLinks = listOf(
+            navDeepLink<MapsRoute>(
+                basePath = url
+            )
+        )
+    ) {
         val viewModel = hiltViewModel<MapsViewModel>()
         val state by viewModel.state.collectAsStateWithLifecycle()
         MapsScreen(state = state)
